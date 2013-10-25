3a56bc68a2c6d17d6a47322baac2a45f9feb36ea
Daniele Guido <gui.daniele@gmail.com>
2013-10-25 15:32:49 +0200

    local_settings updated

--- frontcast/local_settings.sample.py
+++ frontcast/local_settings.sample.py
@@ -1,6 +1,7 @@
+FRONTCAST_HOME = "/path/to/frontcast"
 
 DB_BACKEND	= 'django.db.backends.sqlite3'
-DB_NAME	= '/path/to/frontcast/sqlite/frontcast.db'
+DB_NAME	= '%s/sqlite/frontcast.db' % FRONTCAST_HOME
 DB_USER = ''
 DB_PASS = ''
 DB_HOST = ''
@@ -11,28 +12,28 @@ AUTH_LDAP_SERVER_URI = ""
 AUTH_LDAP_USER_DN_TEMPLATE = ""
 
 # media and storage root differs: media is the path where uploaded stuff goes, storage allow FTP access
-MEDIA_ROOT = '/path/to/frontcast/media'
-STORAGE_ROOT = '/external/path/to/frontcast/storage/public'
-STORAGE_ROOT_PROTECTED = '/external/path/to/frontcast/storage/protected' # available only for logged in sessions
+MEDIA_ROOT = '%s/media' % FRONTCAST_HOME
+STORAGE_ROOT = '/external%s/storage/public' % FRONTCAST_HOME
+STORAGE_ROOT_PROTECTED = '/external%s/storage/protected' % FRONTCAST_HOME # available only for logged in sessions
 
 SECRET_KEY = 'secret very secret key'
 
 TEMPLATE_DIRS = (
-	'/path/to/frontcast/templates',
+	'%s/templates' % FRONTCAST_HOME,
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
 
 STATICFILES_DIRS = (
-    '/path/to/frontcast/static',
+    '%s/static' % FRONTCAST_HOME,
 )
 
-STATIC_ROOT = '/path/to/frontcast/static/assets'
+STATIC_ROOT = '%s/static/assets' % FRONTCAST_HOME
 STATIC_URL = '/frontcast/static/'
 
 LOCALE_PATHS = (
-	'/path/to/frontcast/locale',
+	'%s/locale' % FRONTCAST_HOME,
 )
 
 LANGUAGES = (
@@ -44,7 +45,7 @@ LANGUAGES = (
 LOGIN_URL = '/login/'
 LOGOUT_URL = '/logout/'
 
-GLUE_LOG_PATH = '/path/to/frontcast/logs/glue.log'
+GLUE_LOG_PATH = '%s/logs/glue.log' % FRONTCAST_HOME
 GLUE_ACCESS_DENIED_URL ='/api/access-denied/'
 
 
