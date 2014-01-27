e26e6a81493d0e5396756dadbb95837bc6778710
Daniele Guido <gui.daniele@gmail.com>
2014-01-27 10:56:01 +0100

    language defautl set to english

--- frontcast/settings.py
+++ frontcast/settings.py
@@ -33,7 +33,7 @@ TIME_ZONE = 'Europe/Rome'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'it-it'
+LANGUAGE_CODE = 'en-EN'
 
 SITE_ID = 1
 
