6ed3477fb5c9298f7346e5bec5adf60f54f53638
Daniele Guido <gui.daniele@gmail.com>
2015-01-30 14:37:22 +0100

    adding views and starting index controller

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,4 +1,6 @@
 'use strict';
+// require a STATIC_URL global variable set
+var STATIC_URL = STATIC_URL || '';
 
 angular.module('frontcast', [
   'ngRoute'
@@ -6,7 +8,51 @@ angular.module('frontcast', [
   .config(function ($routeProvider, $httpProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'src/views/index.html',
+        templateUrl: STATIC_URL + 'src/views/index.html',
+        controller: 'indexCtrl'
+      })
+
+      // view list, create, view, edit a "document"
+      .when('/d', {
+        templateUrl: STATIC_URL + 'src/views/index.html',
+        controller: 'indexCtrl'
+      })
+      .when('/d/new', {
+        templateUrl: STATIC_URL + 'src/views/index.html',
+        controller: 'indexCtrl'
+      })
+      .when('/d/:id', {
+        templateUrl: STATIC_URL + 'src/views/index.html',
+        controller: 'indexCtrl'
+      })
+      .when('/d/:id/edit', {
+        templateUrl: STATIC_URL + 'src/views/index.html',
+        controller: 'indexCtrl'
+      })
+      .when('/d/:id/props', {
+        templateUrl: STATIC_URL + 'src/views/index.html',
+        controller: 'indexCtrl'
+      })
+
+      // view list, create, view, edit a "working document"
+      .when('/w', {
+        templateUrl: STATIC_URL + 'src/views/index.html',
+        controller: 'indexCtrl'
+      })
+      .when('/w/new', {
+        templateUrl: STATIC_URL + 'src/views/index.html',
+        controller: 'indexCtrl'
+      })
+      .when('/w/:id', {
+        templateUrl: STATIC_URL + 'src/views/index.html',
+        controller: 'indexCtrl'
+      })
+      .when('/w/:id/edit', {
+        templateUrl: STATIC_URL + 'src/views/index.html',
+        controller: 'indexCtrl'
+      })
+      .when('/w/:id/edit', {
+        templateUrl: STATIC_URL + 'src/views/index.html',
         controller: 'indexCtrl'
       })
 
---
+++ client/src/js/controllers/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontcast.controller:indexCtrl
+ * @description
+ * # indexCtrl
+ * Controller of the frontcast layout.
+ */
+angular.module('frontcast')
+  .controller('indexCtrl', function($scope, $log, $routeParams) {
+    $log.debug('indexCtrl loaded.');
+  });
